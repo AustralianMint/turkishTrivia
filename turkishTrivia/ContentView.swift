c4513bcb0cf237676fe60078810ae317ed12da0d
AustralianMint <thomas.frey@code.berlin>
2023-11-30 15:58:17 +0300

    Initial Commit

---
+++ turkishTrivia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  turkishTrivia
+//
+//  Created by Thomas Frey on 30.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ turkishTrivia/turkishTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  turkishTriviaApp.swift
+//  turkishTrivia
+//
+//  Created by Thomas Frey on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct turkishTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
